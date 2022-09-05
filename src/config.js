f9b1ef84cdac62e9180b4bf4d6fc7ff7c75d3093
xiller228 <110915645+xiller228@users.noreply.github.com>
2022-09-05 23:10:56 +0300

    v6.5.23

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { env } from "./app/setup/tg.js";
 
-export const VERSION = [6, 5, 23],
+export const VERSION = [6, 5, 23], //
   PORT = !env.xillerPC ? 3001 : Number(Date.now().toString().substring(9)),
   Plugins = [
     "html", // Сайт
