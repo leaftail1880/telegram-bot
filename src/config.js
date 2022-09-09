ad26ddba0a31c6003226a82df7a6ba550a8bd44a
xiller228 <110915645+xiller228@users.noreply.github.com>
2022-09-09 21:40:21 +0300

    v6.6.1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { env } from "./app/setup/tg.js";
 
-export const VERSION = [6, 6, 0], //
+export const VERSION = [6, 6, 1], //
   PORT = !env.xillerPC ? 3001 : Number(Date.now().toString().substring(9)),
   Plugins = [
     "html", // Сайт
--- src/vendor/html/index.js
+++ src/vendor/html/index.js
@@ -33,45 +33,47 @@ import("./changelog.js").then(async () => {
     />
     <meta property="og:url" content="https://xiller-tg-bot.onrender.com/" />
     <!-- Open Graph: Article -->
-    body {
-      background: rgb(25, 77, 142);
-      background: linear-gradient(
-        304deg,
-        rgba(25, 77, 142, 1) 0%,
-        rgba(26, 7, 32, 1) 100%
-      );
-      color: rgb(228, 213, 236);
-      /*background-color: rgb(48, 44, 56);*/
-      padding: 2%;
-    }
-    li {
-      list-style-type: "- ";
-      right: 4pt;
-      bottom: 10px;
-      margin-top: 3px;
-    }
-    ul {
-      display: block;
-      list-style-type: disc;
-      margin-block-start: -1em;
-      margin-block-end: 30px;
-      margin-inline-start: 0px;
-      margin-inline-end: 0px;
-      padding-inline-start: 20px;
-    }
-    h4 {
-      left: 10px;
-    }
-    h3 {
-      top: 0px;
-      display: block;
-      font-size: 1.17em;
-      margin-block-start: 10px;
-      margin-block-end: -5px;
-      margin-inline-start: 0px;
-      margin-inline-end: 0px;
-      font-weight: bold;
-    }
+    <style>
+      body {
+        background: rgb(25, 77, 142);
+        background: linear-gradient(
+          304deg,
+          rgba(25, 77, 142, 1) 0%,
+          rgba(26, 7, 32, 1) 100%
+        );
+        color: rgb(228, 213, 236);
+        /*background-color: rgb(48, 44, 56);*/
+        padding: 2%;
+      }
+      li {
+        list-style-type: "- ";
+        right: 4pt;
+        bottom: 10px;
+        margin-top: 3px;
+      }
+      ul {
+        display: block;
+        list-style-type: disc;
+        margin-block-start: -1em;
+        margin-block-end: 30px;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        padding-inline-start: 20px;
+      }
+      h4 {
+        left: 10px;
+      }
+      h3 {
+        top: 0px;
+        display: block;
+        font-size: 1.17em;
+        margin-block-start: 10px;
+        margin-block-end: -5px;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+      }
+    </style>
     <title>Кобольдя</title>
   </head>
   <body>
