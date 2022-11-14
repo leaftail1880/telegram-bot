bd72346577ef96ab7522e18b48b0ab939798d13c
xiller228 <110915645+xiller228@users.noreply.github.com>
2022-11-14 23:47:15 +0300

    v8.1.9.01

---
+++ src/modules/Command/public/ping.js
@@ -0,0 +1,40 @@
+import {
+    Command
+} from "../../../lib/Class/Cmd.js";
+import {
+    d
+} from "../../../lib/Class/Utils.js";
+import {
+    Xitext
+} from "../../../lib/Class/Xitext.js";
+import {
+    env
+} from "../../../lib/launch/tg.js";
+import {
+    data
+} from "../../../lib/SERVISE.js";
+
+new Command(
+    {
+        name: "ping",
+        description: "Скорость ответа",
+        permisson: 0,
+        type: "all",
+        hide: true,
+    },
+    (ctx) => {
+        const data = ~~(Date.now()/1000)
+        ctx.reply(
+            ...new Xitext()
+            .text(`Пинг `)
+            .bold(data - ctx.message.date)
+            .text("\n\nДата: ")
+            .bold(data)
+            .text("\nДата сообщения: ")
+            .bold(ctx.message.date)
+            ._.build({
+                disable_web_page_preview: true
+            })
+        );
+    }
+);
