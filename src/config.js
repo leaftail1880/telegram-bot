2fc6fd9985c04576a4a0a83e1637c754eab998a3
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2023-02-11 20:08:49 +0300

    9.6.3

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	// Keep it one line for autoreplace from leafs/commit.js
-	version: [9, 6, 2],
+	version: [9, 6, 3],
 
 	command: {
 		/**
--- src/modules/Command/public/nonotify.js
+++ src/modules/Command/public/nonotify.js
@@ -16,6 +16,7 @@ new Command(
 			ctx.reply(fmt`${l} снова с нами!`, {
 				reply_to_message_id: ctx.message.message_id,
 				allow_sending_without_reply: true,
+				disable_web_page_preview: true,
 			});
 
 			delete members[ctx.from.id];
@@ -23,9 +24,26 @@ new Command(
 			ctx.reply(fmt`${l} отдыхает от чата.`, {
 				disable_web_page_preview: true,
 			});
-			members[ctx.from.id] = input ? input : "нельзя";
+			members[ctx.from.id] = input ? input : "так спешил(а), что даже не сказал(а) почему...";
 		}
 
 		tables.groups.set(ctx.chat.id, ctx.data.group);
+	}
+);
+
+new Command(
+	{
+		name: "status",
+		description: "Показывает, кто в этом чате не пингуется и почему",
+	},
+	(ctx) => {
+		const members = ctx.data.group.cache.silentMembers;
+		if (Object.keys(members).length < 1) return ctx.reply("Ура, никто не спит.");
+		let text = fmt`Спят здесь эти:`;
+		for (const [who, why] of Object.entries(members)) {
+			const user = tables.users.get(who);
+			text = fmt`${text}\n ${link(util.getName(user), u.httpsUserLink(user.static.nickname))} потому что ${why}`;
+		}
+		ctx.reply(text, { disable_notification: true, disable_web_page_preview: true });
 	}
 );
