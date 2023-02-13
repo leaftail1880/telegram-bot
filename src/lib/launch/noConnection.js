1271e45e421b7d50bf4148ab002c48c596a01bde
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2023-02-13 19:02:06 +0300

    9.6.6

--- src/lib/launch/noConnection.js
+++ src/lib/launch/noConnection.js
@@ -1,63 +1,79 @@
 import config from "../../config.js";
-import { bot, data, database, Service } from "../../index.js";
-import { XTimer } from "../Class/XTimer.js";
+import {
+  bot,
+  data,
+  database,
+  Service
+} from "../../index.js";
+import {
+  XTimer
+} from "../Class/XTimer.js";
 import styles from "../styles.js";
 
 const Connect = {
-	/** @type {NodeJS.Timer} */
-	Interval: null,
-	/** @type {Function} */
-	Resolve: null,
-	/** @type {Promise<void>} */
-	Promise: null,
+  /** @type {NodeJS.Timer} */
+  Interval: null,
+  /** @type {Function} */
+  Resolve: null,
+  /** @type {Promise<void>} */
+  Promise: null,
 };
 
 const ErrorLog = new XTimer(config.NoConnectionLogCooldown);
 
 /**
- *
- * @param {string} [type]
- * @returns
- */
+*
+* @param {string} [type]
+* @returns
+*/
 export async function noConnection(type) {
-	if (data.isLaunched && !data.isStopped) {
-		data.isStopped = true;
-		bot.stop("NOCONNECTION");
-	}
-	if (!database.isClosed) database.Close();
+  if (data.isLaunched && !data.isStopped) {
+    data.isStopped = true;
+    bot.stop("NOCONNECTION");
+  }
+  if (!database.isClosed) database.Close();
 
-	if (ErrorLog.isExpired()) {
-		console.log(styles.noConnection(`Нет подключения к интернету ${type ? `${type}` : ""}`));
-	}
+  if (ErrorLog.isExpired()) {
+    console.log(styles.noConnection(`Нет подключения к интернету ${type ? `${type}`: ""}`));
+  }
 
-	if (!Connect.Interval) {
-		Connect.Interval = setInterval(timer, config.ReconnectTimerWaitTime * 1000);
-	}
+  if (!Connect.Interval) {
+    Connect.Interval = setInterval(timer, config.ReconnectTimerWaitTime * 1000);
+  }
 
-	if (!Connect.Promise) {
-		Connect.Promise = new Promise((resolve) => {
-			Connect.Resolve = resolve;
-		});
-	}
-	return Connect.Promise;
+  if (!Connect.Promise) {
+    Connect.Promise = new Promise((resolve) => {
+      Connect.Resolve = () => {
+        resolve()
+        delete Connect.Promise
+        delete Connect.Resolve
+      };
+    });
+  }
+  return Connect.Promise;
 }
 
 async function timer() {
-	try {
-		if (!data.isStopped) return;
-		await bot.telegram.getMe();
+  if (!data.isStopped) return;
 
-		await database.Reconnect();
-		Service.safeBotLauch();
+  try {
+    // Checking if external services are avaible
+    await bot.telegram.getMe();
+  } catch (e) {
+    if (e.name === "FetchError") return;
 
-		console.log(styles.connectionResolved("Подключение восстановлено!"));
+    // Oops, not a network error
+    throw e
+  }
 
-		clearInterval(Connect.Interval);
-		delete Connect.Interval;
+  await database.Reconnect();
+  Service.safeBotLauch();
 
-		Connect.Resolve();
-		Connect.Promise;
-		delete Connect.Promise;
-		delete Connect.Resolve;
-	} catch (e) {}
+  console.log(styles.connectionResolved("Подключение восстановлено!"));
+
+  clearInterval(Connect.Interval);
+  delete Connect.Interval;
+
+  // Call waiting for connection resolve functiom
+  Connect.Resolve();
 }
