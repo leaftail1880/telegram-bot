56e8ba4c518d33e24236acf21e2a5470c35c1b84
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-07-01 21:31:43 +0300

    9.13.21 AAAAAAA

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /usr/bot
 COPY package.json .
 COPY yarn.lock .
 COPY src src
+COPY patches patches
 
 RUN npm -g install corepack@0.31
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	// Keep it one line for autoreplace from leafs/commit.js
-	version: [9, 13, 20],
+	version: [9, 13, 21],
 
 	command: {
 		/**
