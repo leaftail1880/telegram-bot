8674d7b67106f8ca02ee26d898978e26873694cd
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-07-01 21:57:33 +0300

    9.13.24 Dont use global proxy

--- src/index.js
+++ src/index.js
@@ -3,24 +3,7 @@ export * from "./lib/launch/telegraf.js";
 export * from "./lib/launch/database.js";
 export * from "./lib/Service.js";
 
-import { socksDispatcher } from "fetch-socks";
 import { Service } from "./lib/Service.js";
-
-const proxyUrl = process.env.DISCORD_SOCKS_PROXY_URL;
-
-import undici from "undici";
-
-if (proxyUrl) {
-	globalThis.fetch = undici.fetch;
-	const { hostname, port } = new URL(proxyUrl);
-	console.log(`Using global proxy at hostname=${hostname}, port=${port}`);
-	const agent = socksDispatcher({
-		type: 5,
-		host: hostname,
-		port: parseInt(port),
-	});
-	undici.setGlobalDispatcher(agent);
-}
 
 // Global error handlers
 process.on("unhandledRejection", Service.handlers.processError);
