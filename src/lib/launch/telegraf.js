736c127a811b49b75bf2c8649537ff01ff2133a8
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2026-06-07 19:49:49 +0300

    update telegraf.js

--- src/lib/launch/telegraf.js
+++ src/lib/launch/telegraf.js
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import { Telegraf } from "telegraf-hardened";
 import { fetchWithProxy } from "../proxy.js";
 export * from "telegraf-hardened";
@@ -6,7 +7,7 @@ export * from "telegraf-hardened/format";
 export * from "telegraf-hardened/future";
 
 const ENV_PATH = process.argv[2] ?? ".env";
-process.loadEnvFile(ENV_PATH);
+if (existsSync(ENV_PATH)) process.loadEnvFile(ENV_PATH);
 
 if (!process.env.TOKEN || !process.env.DB_TOKEN || !process.env.DB_REPO) {
 	throw new Error(`No TOKEN in env on ${ENV_PATH} found!`);
